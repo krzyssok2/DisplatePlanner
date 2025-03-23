c1438375dfcc478358f7a438ae968a69483ce82e
sokolkrzys1999@gmail.com <sokolkrzys1999@gmail.com>
2025-03-23 17:10:16 +0200

    Add project files.

---
+++ DisplatePlanner/Models/LimitedResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace DisplatePlanner.Models;
+
+public record LimitedResponse(
+    List<PlateData> Data);
+
+public record PlateData(
+    int Id,
+    string Sku,
+    int ItemCollectionId,
+    string Title,
+    string Url,
+    Edition Edition,
+    Images Images);
+
+public record Edition(
+    string StartDate,
+    string EndDate,
+    string Status,
+    int Available,
+    int Size,
+    string Type,
+    string Format
+);
+
+public record Images(
+    Main Main);
+
+public record Main(
+    string Url,
+    int Width,
+    int Height);
---
+++ DisplatePlanner/Models/LuminoResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace DisplatePlanner.Models;
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+public record LuminoResponse(LuminoListings LuminoListings);
+
+public record LuminoListings
+(
+    List<PlateInfo> Data
+);
+
+public record PlateInfo
+(
+    //int internalId,
+    //int externalId,
+    DateTime StartDate,
+    //DateTime finishDate ,
+    //int availableQuantity ,
+    //string status ,
+    //Author author
+    string Title,
+    Image Image
+);
+
+public record Author
+(
+    int Id,
+    string FullName,
+    string Nick,
+    string Avatar,
+    string Url
+);
+
+public record Image(
+    //string x1,
+    string X2
+);
