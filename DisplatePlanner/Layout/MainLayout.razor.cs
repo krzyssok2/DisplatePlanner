c85f85fc38e8c83f62beae623e95bf43cd7637b6
sokolkrzys1999@gmail.com <sokolkrzys1999@gmail.com>
2025-04-07 00:22:50 +0300

    * Dark/White theme * Theme saved into storage

---
+++ DisplatePlanner/Layout/MainLayout.razor.cs
@@ -0,0 +1,34 @@
+﻿using Blazored.LocalStorage;
+
+namespace DisplatePlanner.Layout;
+
+public partial class MainLayout(ILocalStorageService localStorageService)
+{
+    private const string storageThemeKey = "theme";
+
+    private bool isDarkMode;
+    private string SelectedTheme => isDarkMode ? "dark" : "white";
+
+    protected override async Task OnInitializedAsync()
+    {
+        var theme = await localStorageService.GetItemAsync<string>(storageThemeKey);
+
+        if (theme == "dark")
+        {
+            isDarkMode = true;
+        }
+        else
+        {
+            isDarkMode = false;
+        }
+    }
+
+    private async void ToggleTheme()
+    {
+        isDarkMode = !isDarkMode;
+
+        await localStorageService.SetItemAsync(storageThemeKey, SelectedTheme);
+        // LocalStorage.SetItem("darkMode", isDarkMode);
+        // UpdateTheme();
+    }
+}
